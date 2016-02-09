395c1b0cdf2182fc9e394212a90c2ab43f2241ff
altaufique <altaufique@gmail.com>
2016-02-10 06:05:49 +0800

    Small comments in sqlWriteable....

--- app/src/main/java/com/fhb/sqliteapp/DatabaseHelper.java
+++ app/src/main/java/com/fhb/sqliteapp/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.fhb.sqliteapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,6 +12,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "pg.db";
+    public static final int DATABASE_VERSION = 1;
     public static final String TABLE_NAME = "package";
     public static final String COL_1 = "type_ID";
     public static final String COL_2 = "name";
@@ -18,23 +20,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_4 = "gold_qty";
     public static final String COL_5 = "fee";
 
-    public DatabaseHelper(Context context) { //Constructor to create database
-        super(context, DATABASE_NAME, null, 1);
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION); //Constructor to create database. Using 'super' to call parent class and passing the current parameter DATABASE_NAME to create the database
     }
 
+    // Creating database table
     @Override
-    public void onCreate(SQLiteDatabase db) { // method to create table
+    public void onCreate(SQLiteDatabase db) { // method to create table with one parameter of database instance.
         db.execSQL("create table " + TABLE_NAME + " (" + COL_1 + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_2 + " TEXT NOT NULL, " + COL_3 + " TEXT NOT NULL, " + COL_4 + " INTEGER NOT NULL, " + COL_5 + " FLOAT NOT NULL)");
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
-        onCreate(db);
+        //    |
+        //    |
+        // use execSQL to execute SQL query
     }
 
     public boolean insertData(String name, String gold, String gold_qty, String fee){
-        SQLiteDatabase db = this.getWritableDatabase();     // Open database connection. Remember to close by calling close()
+        // Open database connection. Remember to close by calling close()
+        SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, name);
@@ -44,9 +45,37 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         long result = db.insert(TABLE_NAME, null, contentValues);
 
+        // Close the database
+        db.close();
+
         if (result == -1)
             return false; // failed
         else
             return true;
+    }
+
+    public Cursor getAllData () { // Cursor class data type, give access to read write to result
+        // Open database connection. Remember to close by calling close()
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor result = db.rawQuery("select * from " +TABLE_NAME, null);
+        return result;
+    }
+
+    public boolean updateDate(){
+        // Open database connection. Remember to close by calling close()
+        SQLiteDatabase db = this.getWritableDatabase();
+
+
+        return true;
+    }
+
+    // This method is called when database is upgraded like modifying the table structure, adding constraints to database etc.,
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop database table if exist
+        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME);
+        // Create table again
+        onCreate(db);
     }
 }
--- app/src/main/java/com/fhb/sqliteapp/MainActivity.java
+++ app/src/main/java/com/fhb/sqliteapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.fhb.sqliteapp;
 
+import android.app.AlertDialog;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,38 +13,35 @@ import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper myDB;
+    DatabaseHelper myDB; // myDB is DatabaseHelper data type. it is not a class but a referenced to DatabaseHelper method, subclass etc..
 
-    EditText editTextPkgName;
+    EditText editTextPkgName; // ditto
     EditText editTextPkgGold;
     EditText editTextGoldQty;
     EditText editTextPkgFee;
     Button buttonPkgUpdate;
+    Button buttonGetPkg;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) { // This is called when database is created.
+        super.onCreate(savedInstanceState);     // Using 'super' to call parent class (AppCompatActivity) method of onCreate.
         setContentView(R.layout.activity_main);
         myDB = new DatabaseHelper(this);
 
+        // Casting declared Widgets
         editTextPkgName = (EditText)findViewById(R.id.editText_PkgName);
         editTextPkgGold = (EditText)findViewById(R.id.editText_PkgGold);
         editTextGoldQty = (EditText)findViewById(R.id.editText_GoldQty);
         editTextPkgFee = (EditText)findViewById(R.id.editText_PkgFee);
-        buttonPkgUpdate = (Button)findViewById(R.id.button_PkgUpdate); // this var is use for action block
+        buttonPkgUpdate = (Button)findViewById(R.id.button_PkgUpdate); // this var is use in action code in insertData method
+        buttonGetPkg = (Button)findViewById(R.id.button_GetPkg); // this var is use for action code in getPkg method
 
-        updateData();
-
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(
-                                Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(
-                                Stetho.defaultInspectorModulesProvider(this))
-                        .build());
+        callStetho();
+        insertData();
+        getData();
     }
 
-    public void updateData () {
+    public void insertData () {
         buttonPkgUpdate.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -52,12 +51,62 @@ public class MainActivity extends AppCompatActivity {
                                 editTextGoldQty.getText().toString(),
                                 editTextPkgFee.getText().toString());
 
-                        if (isInserted =true)
-                            Toast.makeText(MainActivity.this,"Data is inserted.",Toast.LENGTH_LONG).show();
+                        if (isInserted = true)
+                            Toast.makeText(MainActivity.this, "Success!! Data is inserted.", Toast.LENGTH_LONG).show();
                         else
-                            Toast.makeText(MainActivity.this,"Data is not inserted.",Toast.LENGTH_LONG).show();
+                            Toast.makeText(MainActivity.this, "Failed!! Data is not inserted.", Toast.LENGTH_LONG).show();
                     }
                 }
         );
+    }
+
+    public void getData() {
+        buttonGetPkg.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Get the data from database
+                        Cursor result = myDB.getAllData(); // Cursor data type as a ref to Cursor class.
+
+                        if(result.getCount() == 0) {
+                            // error message
+                            showMessage("ERROR", "No data found!!!");
+                            return;
+                        }
+
+                        // Show the result
+                        // set a variable to hold the string
+                        StringBuffer stringBuffer = new StringBuffer(); //buffer now is empty
+
+                        // Loop the entire data
+                        while (result.moveToNext()) {
+                            stringBuffer.append("Package: " + result.getString(1) +"\n");
+                            stringBuffer.append("Name: " + result.getString(2) +"\n");
+                            stringBuffer.append("Gold_qty: " + result.getString(3) +"\n");
+                            stringBuffer.append("Fee: RM" + result.getString(4) + "\n\n");
+                        }
+                        showMessage("PowerGold Package", stringBuffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage (String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    // Call Stheto library to do database and other resources checking
+    public void callStetho () {
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(
+                                Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(
+                                Stetho.defaultInspectorModulesProvider(this))
+                        .build());
     }
 }
